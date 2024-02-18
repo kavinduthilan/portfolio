b1b9dc56eebfb7dabe69db6c1ba3ca33b421ac46
Kavindu Thilan <kavinduthilan2000@outlook.com>
2024-02-18 16:58:44 +0530

    Added paragraph

--- src/componets/About.js
+++ src/componets/About.js
@@ -9,7 +9,15 @@ export default function About() {
       <h1>About Me</h1>
       <div className="about">
         <div className="bio">
-          
+          <p>
+                        I am a Computer Science undergraduate at University of Ruhuna. I am a
+            highly motivated individual who is passionate about programming and
+            software development. I am always eager to learn new technologies and to
+            take on new challenges. I am a team player who is able to work effectively
+            in a team and I am also capable of working independently. I am a quick
+            learner and I am always willing to take on new opportunities to improve my
+            skills.
+          </p>
         
         </div>
       </div>
